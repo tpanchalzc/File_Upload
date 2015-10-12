72b5960a1a0c148a1d2315655774619837440ce5
dsavlanizc <dsavlani@zerochaos.com>
2015-10-12 14:15:45 +0530

    basic setup

---
+++ File_Upload/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace File_Upload.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts").Include("~/Scripts/bootstrap.js",
+                                         "~/Scripts/jquery-2.1.4.js"));
+            bundles.Add(new StyleBundle("~/Styles").Include("~/Content/bootstrap.css",
+                                                         "~/Content/bootstrap-theme.css"));
+        }
+    }
+}
