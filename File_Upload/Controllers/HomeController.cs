360b226f448f9dce0cda18708221b2cd9513fc6d
dsavlanizc <dsavlani@zerochaos.com>
2015-10-13 18:29:19 +0530

    Read data from excel sheet

--- File_Upload/Controllers/HomeController.cs
+++ File_Upload/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,22 +20,55 @@ namespace File_Upload.Controllers
         }
 
         [HttpPost]
-        public string Upload(HttpPostedFileBase myFile)
+        public ActionResult Upload(HttpPostedFileBase myFile)
         {
-            if (myFile != null && myFile.ContentLength > 0)
+            Application xlApp = null;
+            Workbook book = null;
+            try
             {
-                string fileName = Path.GetFileName(myFile.FileName);
-                string path = Path.Combine(Server.MapPath("~/App_Data/"), fileName);
-                myFile.SaveAs(path);
-                string connectionString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0;HDR=YES\";", path);
-
-                using (OleDbConnection conn = new OleDbConnection(connectionString))
+                //myFile = Request.Files[0];
+                if (myFile != null && myFile.ContentLength > 0)
                 {
-                    conn.Open();
+                    string fileName = Path.GetFileName(myFile.FileName);
+                    string path = Path.Combine(Server.MapPath("~/App_Data/"), fileName);
+                    myFile.SaveAs(path);
+
+                    xlApp = new Application();
+                    book = xlApp.Workbooks.Open(path);
+
+                    string res = "Total worksheets are:" + book.Worksheets.Count;
+
+                    List<List<string>> data = new List<List<string>>();
+
+                    Worksheet ws = book.Worksheets[1];
+                    for (int i = 5; i < 50; i++)
+                    {
+                        List<string> row = new List<string>();
+                        for (int j = 5; j < 50; j++)
+                        {
+                            string tmp = ""+(ws.Cells[i, j] as Microsoft.Office.Interop.Excel.Range).Value;
+                            row.Add(tmp);
+                        }
+                        data.Add(row);
+                    }
+                    Marshal.ReleaseComObject(ws);
+                    book.Close();
+                    return PartialView("SheetDataView", data);
+                    //return Content(res);
                 }
-                return "success";
+                else return Content("failed");
             }
-            return "failed";
+            catch (Exception ex)
+            {
+                return Content(ex.ToString());
+            }
+            finally
+            {
+                Marshal.ReleaseComObject(book);
+                Marshal.ReleaseComObject(xlApp);
+                book = null;
+                xlApp = null;
+            }
         }
     }
 }
