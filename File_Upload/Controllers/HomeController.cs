ffd8172635a01e140f7b2011c2dd5b3ebb023b58
dsavlanizc <dsavlani@zerochaos.com>
2015-10-13 12:04:39 +0530

    file upload done

--- File_Upload/Controllers/HomeController.cs
+++ File_Upload/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,25 @@ namespace File_Upload.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public string Upload(HttpPostedFileBase myFile)
+        {
+            if (myFile != null && myFile.ContentLength > 0)
+            {
+                string fileName = Path.GetFileName(myFile.FileName);
+                string path = Path.Combine(Server.MapPath("~/App_Data/"), fileName);
+                myFile.SaveAs(path);
+                string connectionString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0;HDR=YES\";", path);
+
+                using (OleDbConnection conn = new OleDbConnection(connectionString))
+                {
+                    conn.Open();
+                }
+                return "success";
+            }
+            return "failed";
         }
     }
 }
