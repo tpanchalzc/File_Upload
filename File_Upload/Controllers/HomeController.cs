f45f6866c51946f34fe808c719916aab68983109
tpanchalzc <tpanchal@zerochaos.com>
2015-10-13 11:04:34 +0530

    changes with error :D, ps try to solve it

--- File_Upload/Controllers/HomeController.cs
+++ File_Upload/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using File_Upload.Models;
+using System.Web.UI.WebControls;
 
 namespace File_Upload.Controllers
 {
@@ -13,5 +15,54 @@ namespace File_Upload.Controllers
         {
             return View();
         }
+        public ActionResult FileUpload()
+        {
+            return View();
+        }
+
+        public ActionResult FileUpload(HttpPostedFileBase fl)
+        {
+          
+            if (fl.ContentLength > 0)
+            {
+                string fileExt = System.IO.Path.GetExtension(Request.Files["fileUpload"].FileName);
+
+                if(fileExt=="xls")
+                {
+                    string fileLocation =string.Format("{0}/{1}",Server.MapPath("~/ExcelFile"),Request.Files["FileUpload"].FileName);
+                    Request.Files["FileUpload"].SaveAs("~/ExcelFile");
+                   var excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileLocation + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
+                    //connection String for xls file format.
+                    if (fileExt== ".xls")
+                    {
+                        excelConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileLocation + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"";
+                    }
+                    //connection String for xlsx file format.
+                    else if (fileExt == ".xlsx")
+                    {
+
+                        excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileLocation + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
+                    }
+
+                }
+                else
+                { 
+                    ViewBag.Message = "sorry, Please select Excel File";
+                    ViewBag.Bool = false;
+                }
+                ViewBag.Message = "Your File Has been uploaded";
+                ViewBag.Bool = true;
+            }
+            else 
+            {
+                ViewBag.Message = "sorry, File Has not been uploaded";
+                ViewBag.Bool = false;
+            }
+            return View();
+        }
+
+
     }
+
+
 }
